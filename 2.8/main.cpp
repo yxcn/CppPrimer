656fe1594248eb6c546820fb4708b6d48cbbedc1
Yao Xiang <yao-xiang@chromium.org>
2022-05-07 18:44:23 +0800

    2.3, 2.8

---
+++ 2.8/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "\x32\x4d" << std::endl;
+
+    std::cout << '\x32' << '\n';
+    std::cout << '\t' << '\n';
+    std::cout << '\x4d' << '\n';
+}
